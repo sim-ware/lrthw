0e1630b2435eb214d097750417b28b9768eebc2c
Samir Gossain <samir.gossain@gmail.com>
2016-11-28 14:17:54 +0000

    Chapter 1: A Good First Program

---
+++ chap01/ex2.rb
@@ -0,0 +1,29 @@
+#Make your script print another line
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Another line."
+
+#Make your script print only one of the lines
+
+puts "Hello World!"
+"Hello Again"
+"I like typing this."
+"This is fun."
+"Yay! Printing."
+"I'd much rather you 'not'."
+'I "said" do not touch this.'
+
+#Put a # (octothorpe) character at the beginning
+#of a line. What did it do? Try to find out what
+#this character does.
+
+#puts "Hello World!"
+puts "The Octothorpe makes the text editor ignore
+what follows (as code). Useful for making notes in
+code"
