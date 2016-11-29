6a04ae6a3e37c692f757846752c92ef6f4135414
Samir Gossain <samir.gossain@gmail.com>
2016-11-29 15:55:05 +0000

    Chapter 13: Parameters, Unpacking, Variables

---
+++ chap13/studydrillschap13.rb
@@ -0,0 +1,7 @@
+# Q: Write a script that has fewer arguments and one that has more. Make sure
+#    you give the unpacked variables good names.
+
+first, second = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
---
+++ chap13/studydrillschap13b.rb
@@ -0,0 +1,11 @@
+# Q: Write a script that more arguments. Make sure you give the unpacked
+#    variables good names.
+first, second, third, fourth = $stdin.gets.chomp
+
+puts "Your first variable is #{first}."
+puts "Your second variable is #{second}."
+puts "Your third variable is #{third}."
+puts "Your fourth variable is #{fourth}."
+
+# Bit confused here, seems to only allow $stdin.gets.chomp to take only one
+# argument.
