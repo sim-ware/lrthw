50a591562b02745d91dac48b3433af2e5ade9059
Samir Gossain <samir.gossain@gmail.com>
2016-12-04 22:40:27 +0000

    Chapter 39: Dictionaries, Oh Lovely Dictionaries

---
+++ chap39/ex1chap39.rb
@@ -0,0 +1,31 @@
+# create a mapping of state to abbreviation
+states = {
+  'Oregon' => 'OR',
+  'Florida' => 'FL',
+  'California' => 'CA',
+  'New York' => 'NY',
+  'Michigan' => 'MI'
+}
+
+# create a basic set of states and some critics in them
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit'
+  'FL' => 'Jacksonville'
+}
+
+# add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# puts out some cities
+puts '-' * 10
+puts "NY State has #{cities['NY']}"
+puts "OR State has #{cities['OR']}"
+
+# puts some states
+puts '-' * 10
+puts "Michigan's abbreviation is: #{states['Michigan']}"
+puts "Florida's abbreviation is: #{states['Florida']}"
+
+# do it by using the state then cities dict
